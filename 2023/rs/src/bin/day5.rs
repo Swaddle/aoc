94502a03ebc62ac328e625466b33ee129fa22356
Swaddle <meswaddle@gmail.com>
2023-12-10 14:26:01 +1100

    feat: transform func

--- 2023/rs/src/bin/day5.rs
+++ 2023/rs/src/bin/day5.rs
@@ -10,6 +10,22 @@ use nom::{
     character::complete::line_ending,
 };
 
+type Transformation = (u64, u64, u64);
+
+fn gen_affine_transform(transformations: Vec<Transformation>) -> impl Fn(u64) -> u64 {
+    move |source_value| {
+        transformations.iter().find_map(|&(target_range_start, source_range_start, n_steps)| {
+            let source_range_end = source_range_start + n_steps as u64;
+            let target_range_end = target_range_start + n_steps as u64;
+            if source_value >= source_range_start && source_value < source_range_end {
+                Some(target_range_start + (source_value - source_range_start))
+            } else {
+                None
+            }
+        }).unwrap_or(source_value)
+    }
+}
+
 
 fn parse_seeds(input: &str) ->  IResult<&str, (&str, Vec<u64>) > {
     tuple(
@@ -23,17 +39,21 @@ fn parse_seeds(input: &str) ->  IResult<&str, (&str, Vec<u64>) > {
     )(input)
 }
 
-// pub fn parse_seeds(input: &str) ->  IResult<&str, Vec<u64>> {
-//     // nom parser to get a row of text from a string w format eg:
-//     // seeds:  2 3 4 
-
-// }
-
 
 fn main() {
     // --snip--
-    let contents = fs::read_to_string("../data/5.txt").unwrap();
-    let (remaining, (_label, seeds)) = parse_seeds(&contents).unwrap();
-    println!("{:?}", seeds);
-    println!("{}", remaining)
+
+    let transformations = vec![(50, 98, 2), (30, 70, 3)]; // Example transformations
+    let affine_transform = gen_affine_transform(transformations);
+    let source_values = vec![99, 65, 40];
+
+    for source_value in vec![99, 98, 65, 40] {
+        println!("{} -> {}", source_value, affine_transform(source_value));
+    }
+
+
+    // let contents = fs::read_to_string("../data/5.txt").unwrap();
+    // let (remaining, (_label, seeds)) = parse_seeds(&contents).unwrap();
+    // println!("{:?}", seeds);
+    // println!("{}", remaining)
 }
