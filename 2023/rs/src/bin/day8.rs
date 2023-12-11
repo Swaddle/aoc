31bc0425133df9e108a9d7b07c6de129fda7aae8
Swaddle <meswaddle@gmail.com>
2023-12-11 22:07:36 +1100

    feat: infinite loop lol

--- 2023/rs/src/bin/day8.rs
+++ 2023/rs/src/bin/day8.rs
@@ -2,7 +2,7 @@ use nom::{
     branch::alt, bytes::complete::tag, bytes::complete::take_while,
     character::complete::line_ending, multi::many1, sequence::tuple, IResult,
 };
-
+use std::collections::HashMap;
 use std::fs;
 
 fn parse_instructions(input: &str) -> IResult<&str, Vec<&str>> {
@@ -10,7 +10,7 @@ fn parse_instructions(input: &str) -> IResult<&str, Vec<&str>> {
 }
 
 fn parse_node(input: &str) -> IResult<&str, (&str, (&str, &str))> {
-    let (remaining, (key, _, _, _, _, left, _,_, right, _, _)) = tuple((
+    let (remaining, (key, _, _, _, _, left, _, _, right, _, _)) = tuple((
         take_while(char::is_alphanumeric),
         tag(" "),
         tag("="),
@@ -31,17 +31,31 @@ fn parse_nodes(input: &str) -> IResult<&str, Vec<(&str, (&str, &str))>> {
     Ok((remaining, vec))
 }
 
-// takes the map, a key, and an instruction, returns the next key
-// fn step(node_map: &HashMap<&str, (&str, &str)>, key: &str, instruction: &str) -> Option<&str> {
-//     match instruction {
-//         "L" => node_map.get(key).map(|(left, _)| *left),
-//         "R" => node_map.get(key).map(|(_, right)| *right),
-//         _ => None,
-//     }
-// }
+//takes the map, a key, and an instruction, returns the next key
+fn step<'a>(node_map: &'a HashMap<&'a str, (&'a str, &'a str)>, key: &'a str, instruction: &'a str) -> Option<&'a str>  {
+    match instruction {
+        "L" => node_map.get(key).map(|(left, _)| *left),
+        "R" => node_map.get(key).map(|(_, right)| *right),
+        _ => None,
+    }
+}
 
 fn main() {
     let contents = fs::read_to_string("../data/8.txt").unwrap();
-    let (_remaining, instructions) = parse_instructions(&contents).unwrap();
-    let _instrunction_cycle = instructions.iter().cycle();
+    let (remaining, instructions) = parse_instructions(&contents).unwrap();
+    let (_, nodes) = parse_nodes(&remaining).unwrap();
+    let mut _instrunction_cycle = instructions.iter().cycle();
+    println!("{:?}", instructions);
+    println!("{:?}", nodes);
+    
+    let key = "AAA";
+    
+    let node_map: HashMap<&str, (&str, &str)> = nodes.into_iter().collect();
+    
+    let mut count = 0;
+    while key != "ZZZ" {
+        count += 1;
+        let Some(key) = step(&node_map, key, _instrunction_cycle.next().unwrap()) else { break };
+        println!("{}", key);
+    }
 }
