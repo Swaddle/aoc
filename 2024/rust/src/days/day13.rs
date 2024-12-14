417c7ad92708210ac1375faf5accee6ce589981a
Fennecs <116326048+StrongFennecs@users.noreply.github.com>
2024-12-14 14:31:09 +1100

    fix: blah

--- 2024/rust/src/days/day13.rs
+++ 2024/rust/src/days/day13.rs
@@ -10,10 +10,13 @@ use nom::{
 
 type Claw = (i64, i64, i64, i64, i64, i64);
 
+#[inline(always)]
 pub fn parse_i64(input: &str) -> IResult<&str, i64> {
     map_res(digit1, str::parse::<i64>)(input)
 }
 
+
+#[inline(always)]
 pub fn parse_a_row(input: &str) -> IResult<&str, (i64, i64)> {
     let (remaining, (_, a, _, b, _)) = tuple((
         tag("Button A: X+"),
@@ -26,6 +29,7 @@ pub fn parse_a_row(input: &str) -> IResult<&str, (i64, i64)> {
     Ok((remaining, (a, b)))
 }
 
+#[inline(always)]
 pub fn parse_b_row(input: &str) -> IResult<&str, (i64, i64)> {
     let (remaining, (_, a, _, b, _)) = tuple((
         tag("Button B: X+"),
@@ -38,6 +42,7 @@ pub fn parse_b_row(input: &str) -> IResult<&str, (i64, i64)> {
     Ok((remaining, (a, b)))
 }
 
+#[inline(always)]
 pub fn parse_prize_row(input: &str) -> IResult<&str, (i64, i64)> {
     let (remaining, (_, a, _, b, _)) = tuple((
         tag("Prize: X="),
@@ -50,64 +55,69 @@ pub fn parse_prize_row(input: &str) -> IResult<&str, (i64, i64)> {
     Ok((remaining, (a, b)))
 }
 
+#[inline(always)]
 pub fn parse_block(input: &str) -> IResult<&str, Claw> {
     let (remaining, ((a, b), (c, d), (p1, p2))) =
         tuple((parse_a_row, parse_b_row, parse_prize_row))(input)?;
 
     Ok((
         remaining,
-        (a, b, c, d, 10000000000000 + p1, 10000000000000 + p2),
+        //(a, b, c, d, 10000000000000 + p1, 10000000000000 + p2),
+        (a, b, c, d, p1, p2),
     ))
 }
 
+#[inline(always)]
 pub fn parse_input(input: &str) -> IResult<&str, Vec<Claw>> {
     let (rem, blocks) = separated_list0(tag("\n"), parse_block)(input)?;
     Ok((rem, blocks))
 }
 
-pub fn solve_block(block: Claw) -> Option<(i64, i64)> {
+pub fn solve_block(block: Claw) -> i64 {
     let (x1, y1, x2, y2, p1, p2) = block;
-
     let denom = (x1 * y2 - x2 * y1);
+    
     if denom == 0 {
-        return None;
+        return 0;
     }
     let x = (y2 * p1 - x2 * p2);
     let y = (x1 * p2 - y1 * p1);
-
     let xrem = x % denom;
     let yrem = y % denom;
 
+    if xrem != 0 || yrem != 0 {
+        return 0;
+    }
     let x0 = (x / denom);
     let y0 = (y / denom);
 
     // check if remainder i
-
-    if x0 >= 0 && y0 >= 0 && xrem == 0 && yrem == 0 {
-        return Some((x0, y0));
+    if x0 >= 0 && y0 >= 0 {
+        return 3 * x0 + y0;
     }
-    None
+    
+    0
 }
 
 pub fn p1() -> u64 {
+    let t0 = std::time::Instant::now();
     let input = std::fs::read_to_string("../data/13.txt.fen").unwrap();
     let (rem, blocks) = parse_input(&input).unwrap();
+    
+    let t1 = t0.elapsed(); 
+    println!("parsing {:?}", t1);
+
     let sols = blocks
         .iter()
         .map(|block| solve_block(*block))
-        .collect::<Vec<Option<(i64, i64)>>>();
-
-    println!("{:?}", sols);
-
+        .collect::<Vec<i64>>();
     // sum it all up
-    let sol = sols
-        .iter()
-        .map(|p| match p {
-            Some((x, y)) => 3 * x + y,
-            None => 0,
-        })
-        .sum::<i64>();
 
+    let sol: i64 = sols.iter().sum();
+
+    let t1 = t0.elapsed();
     println!("{:?}", sol);
+    println!("elapsed: {:?}", t1);
+
     return 0;
 }
