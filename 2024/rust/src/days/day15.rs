a11fdf0281dbe06e2c76bf6584a451e35f26fbd0
Fennecs <116326048+StrongFennecs@users.noreply.github.com>
2024-12-18 12:21:51 +1100

    chore: init 15

---
+++ 2024/rust/src/days/day15.rs
@@ -0,0 +1,23 @@
+use std::collections::HashMap;
+
+pub struct Robot {
+    pub x: i32, 
+    pub y: i32,
+}
+
+pub fn free_space(grid: &HashMap<(i32, i32), char>, x: i32, y: i32) -> bool {
+    return true;
+}
+
+pub fn p1() -> u64 {
+
+    // hashmap of coord to box/wall/empty space
+    // box = "O'
+    // wall = "#"
+    // empty space = "."
+    let mut grid = HashMap::new();
+
+}
+
+
+
